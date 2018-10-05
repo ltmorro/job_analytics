4878ef0b8cdae1d005e8b2333a15b5493717641f
ltmorro <ltmorro@g.clemson.edu>
2018-10-05 18:14:24 -0400

    Updated scrape to two functions

--- analysis.py
+++ analysis.py
@@ -2,15 +2,23 @@ import pandas as pd
 from collections import Counter
 import string
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_top(df, title, x, y):
-    plt.style.use("seaborn-dark")
+def plot_top(df, title):
+    plt.style.use("seaborn")
 
     df.columns=["Jobs"]
-    df.sort_values(by="Jobs", ascending=False).plot(kind='bar')
+    df= df.sort_values(by="Jobs", ascending=False)
+    top=df.plot(kind='bar')
     plt.title(title)
-    plt.xlabel(x)
-    plt.ylabel(y)
+    plt.ylabel("Percentage")
+    for idx, label in enumerate(list(df.index)):
+        for acc in df.columns:
+            value = np.round(df.ix[idx][acc],decimals=2)
+            top.annotate(value,
+                        (idx, value),
+                         xytext=(0, 0),horizontalalignment="center",
+                         textcoords='offset points')
 
 def plot_all(program, analysis, distributed, database, location):
     plt.style.use("seaborn")
@@ -19,19 +27,19 @@ def plot_all(program, analysis, distributed, database, location):
     axes[0,0].set_ylabel("Percentage")
     axes[0,0].set_title("Top Programming Languages")
     #axes[0,0].ylim(0, 100)
-    program.plot(kind='bar', ax=axes[0,0])
+    program.sort_values(by="Percent", ascending=False).plot(kind='bar', ax=axes[0,0])
 
     axes[0,1].set_ylabel("Percentage")
     axes[0,1].set_title("Top Analysis Tools")
-    analysis.plot(kind='bar', ax=axes[0,1])
+    analysis.sort_values(by="Percent", ascending=False).plot(kind='bar', ax=axes[0,1])
 
     axes[1,0].set_ylabel("Percentage")
     axes[1,0].set_title("Top Distributed Frameworks")
-    distributed.plot(kind='bar', ax=axes[1,0])
+    distributed.sort_values(by="Percent", ascending=False).plot(kind='bar', ax=axes[1,0])
 
     axes[1,1].set_ylabel("Percentage")
     axes[1,1].set_title("Top Database Languages")
-    database.plot(kind='bar', ax=axes[1,1])
+    database.sort_values(by="Percent", ascending=False).plot(kind='bar', ax=axes[1,1])
 
     plt.tight_layout(rect=[0, 0.03, 1, 0.95], pad=0.4, w_pad=1.5, h_pad=1.0)
 
@@ -68,7 +76,7 @@ def get_top_words(location):
                     'MongoDB':top_words['mongodb']}
 
     python_libs = {'Tensorflow':top_words['tensorflow'], 'Matplotlib':top_words['matplotlib'], 'Scikit':top_words['scikit'],
-                   'Numpy':top_words['numpy'], 'Pandas':top_words['pandas'], 'Keras':top_words['keras'], 'NLTK':top_words['nltk']}
+                   'Numpy':top_words['numpy'], 'Pandas':top_words['pandas'], 'Keras':top_words['keras'], 'NLTK':top_words['nltk'], 'Pyspark':top_words['pyspark']}
 
     language = pd.DataFrame.from_dict(program_lang, orient="index")
     language.columns = ["Percent"]
@@ -92,13 +100,77 @@ def get_top_words(location):
     plot_all(language, analysis, distributed, database, location)
     plt.show()
 
+def nationwide(top_cites):
+    top_words = Counter()
+    num_jobs = 0
+    for city in top_cites:
+        job_df = pd.read_csv(city+".csv", encoding="ISO-8859-1")
+
+        for summary in job_df["content"]:
+            num_jobs += 1
+            for word in summary.split():
+                clean_word = word.translate(str.maketrans("","",string.punctuation)).lower()
+
+                top_words[clean_word] += 1
+
+    program_lang = {'Python':top_words['python'], 'R':top_words['r'],
+                    'Java':top_words['java'], 'C':top_words['c'],
+                    'Ruby':top_words['ruby'], 'Perl':top_words['perl'],
+                    'Matlab':top_words['matlab'], 'JavaScript':top_words['javascript'],
+                    'Scala': top_words['scala']}
+
+    analysis_tools = {'Excel':top_words['excel'],  'Tableau':top_words['tableau'],
+                        'D3.js':top_words['d3.js'], 'SAS':top_words['sas'],
+                        'SPSS':top_words['spss'], 'D3':top_words['d3']}
+
+    distributed_tools = {'Hadoop':top_words['hadoop'], 'MapReduce':top_words['mapreduce'],
+                'Spark':top_words['spark'], 'Pig':top_words['pig'],
+                'Hive':top_words['hive'], 'Shark':top_words['shark'],
+                'Oozie':top_words['oozie'], 'ZooKeeper':top_words['zookeeper'],
+                'Flume':top_words['flume'], 'Mahout':top_words['mahout']}
+
+    database_lang = {'SQL':top_words['sql'], 'NoSQL':top_words['nosql'],
+                    'HBase':top_words['hbase'], 'Cassandra':top_words['cassandra'],
+                    'MongoDB':top_words['mongodb']}
+
+    python_libs = {'Tensorflow':top_words['tensorflow'], 'Matplotlib':top_words['matplotlib'], 'Scikit':top_words['scikit'],
+                   'Numpy':top_words['numpy'], 'Pandas':top_words['pandas'], 'Keras':top_words['keras'], 'NLTK':top_words['nltk'], 'Pyspark':top_words['pyspark']}
+
+    language = pd.DataFrame.from_dict(program_lang, orient="index")
+    language.columns = ["Percent"]
+    language["Percent"] = language["Percent"]/num_jobs*100
+    analysis = pd.DataFrame.from_dict(analysis_tools, orient="index")
+    analysis.columns = ["Percent"]
+    analysis["Percent"] = analysis["Percent"]/num_jobs*100
+    distributed = pd.DataFrame.from_dict(distributed_tools, orient="index")
+    distributed.columns = ["Percent"]
+    distributed["Percent"] = distributed["Percent"]/num_jobs*100
+    database = pd.DataFrame.from_dict(database_lang, orient="index")
+    database.columns = ["Percent"]
+    database["Percent"] = database["Percent"]/num_jobs*100
     libs = pd.DataFrame.from_dict(python_libs, orient="index")
     libs.columns = ["Percent"]
     libs["Percent"] = libs["Percent"]/num_jobs*100
-    plot_top(libs, "Top Python Libraries", "Library", "Percentage")
+
+    plot_top(language, "Top Programming Languages")
+    plt.show()
+
+    plot_top(analysis, "Top Analysis Tools")
+    plt.show()
+
+    plot_top(distributed, "Top Distributed Frameworks")
+    plt.show()
+
+    plot_top(database, "Top Database Languages")
+    plt.show()
+
+    plot_top(libs, "Top Python Libraries")
+    plt.show()
 
 if __name__ == "__main__":
-    top_cities = ["New+York", "Chicago", "Boston", "San+Francisco", "Austin", "Houston", "Seattle"]
+    top_cities = ["New+York", "Chicago", "Boston", "San+Francisco", "Austin", "Houston", "Seattle", "Denver"]
     for city in top_cities:
         print("Finding jobs in " + city)
         get_top_words(city)
+
+    nationwide(top_cities)
--- scrape.py
+++ scrape.py
@@ -8,14 +8,14 @@ import nltk.tokenize
 import nltk.corpus
 import string
 jobs_per_page = 50
+base_url = "https://www.indeed.com/"
 
-#downloadJobs pulls all relevant job listings for the job in the location supplied
-def download_jobs(job, location, file_path):
+#this method gets all the job links for a given location
+def get_links(job, location):
     request_data = {"q":job, "l":location, "limit":jobs_per_page, "start":0}
-    base_url = "https://www.indeed.com/"
     #get request
     response = requests.get(base_url+"jobs", params=request_data)
-
+    #create soup so we can parse the html
     soup = BeautifulSoup(response.content, "html.parser")
 
     links = []
@@ -39,48 +39,60 @@ def download_jobs(job, location, file_path):
         time.sleep(1)
 
     links = list(set(links))
+    return links
 
+#downloadJobs pulls all relevant job listings for the set of links and saves them at file_path
+def download_jobs(links, file_path):
     jobs = []
     i=1 #keep track of loop for printing
     for link in links:
         print("Getting Job Posting " + str(i))
         i+=1
+        #send get request to the specfic job link
         response = requests.get(base_url+link)
         url = response.url
-        job = BeautifulSoup(response.content, "lxml")
+        job = BeautifulSoup(response.content, "html.parser")
 
         #stop words to remove
         stop_words = nltk.corpus.stopwords.words('english')
-        #Job Title
         #sometimes the http request breaks so we try until we succeed
+        giveup=0
         while True:
+            #use a try because some responses get corrupted during transmission
             try:
+                #job title
                 title = job.find("h3", attrs={"class":"icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title"}).get_text(separator="\n")
+                #company info
                 company = job.find("div", attrs={"class":"icl-u-lg-mr--sm icl-u-xs-mr--xs"}).get_text()
                 #grab the content and split it in to words
                 content = job.find("div", attrs={"class":"jobsearch-JobComponent-description icl-u-xs-mt--md"}).get_text(separator="\n")
+                #split in to words from the text using nltk tokenize
                 content = nltk.tokenize.word_tokenize(content)
                 #remove stop words which dont add any meaning
-                content = [word for word in content if word not in stop_words and word.isalpha()]
+                content = [word for word in content if word not in stop_words]
             except:
                 print("Trying again")
                 response = requests.get(base_url+link)
                 url = response.url
-                job = BeautifulSoup(response.content, "lxml")
+                job = BeautifulSoup(response.content, "html.parser")
                 time.sleep(1)
+                #we don't want to get stuck in a loop forever
+                if giveup == 5:
+                    break
+                giveup +=1
                 continue
             break
+        #add job to list, we set the content because I only care about existence not frequency
         jobs.append({"url":url, "title":title, "company":company, "content":list(set(content))})
         time.sleep(1)
 
+    #convert the dictionary to a pandas dataframe and save as a csv
     job_df = pd.DataFrame.from_dict(jobs)
-    print(job_df)
-
     job_df.to_csv(location+".csv")
 
 if __name__ == "__main__":
-    #"New+York", "Chicago", "Boston","San+Francisco",
-    top_cities = [ "Austin", "Houston", "Seattle"]
+    top_cities = ["New+York", "Chicago", "Boston","San+Francisco","Houston","Seattle", "Denver"]
     for city in top_cities:
         print("Finding jobs in " + city)
-        download_jobs("data+scientist", city, "")
+        links=get_links("data+scientist", city)
+        download_jobs(links, "")
